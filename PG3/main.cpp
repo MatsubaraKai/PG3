fc374e6ae34c2335c49c0e7da6adee9e385aa4d9
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-10-16 10:50:31 +0900

    確認課題

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,17 +1,46 @@
 #include <stdio.h>
 
-int calc(int a, int b) {
-	return a + b;
+template <typename T>
+T Min(T a, T b) {
+
+	if (a <= b) {
+		return static_cast<T>(a);
+	}
+	else {
+		return static_cast<T>(b);
+	}
 }
-float calc(float d, float e) {
-	return d + e + 10;
+
+template<>
+char Min<char>(char a, char b) {
+
+	printf("”šˆÈŠO‚Í‘ã“ü‚Å‚«‚Ü‚¹‚ñ");
+
+	return 0;
 }
+
 int main() {
-	int x = 114;
-	int y = 514;
-	printf("%d\n", calc(x, y));
-	float l = 3.64f;
-	float m = 3.64f;
-	printf("%f\n", calc(l, m));
+
+	int intA = 10;
+	int intB = 20;
+
+	float floatA = 10.5f;
+	float floatB = 10.2f;
+
+	double doubleA = 10.3;
+	double doubleB = 10.8;
+
+	char charA = 'A';
+	char charB = 'B';
+
+	//intŒ^
+	printf("%d\n", Min<int>(intA, intB));
+	//floatŒ^
+	printf("%f\n", Min<float>(floatA, floatB));
+	//doubleŒ^
+	printf("%lf\n", Min<double>(doubleA, doubleB));
+	//charŒ^
+	Min<char>(charA, charB);
+
 	return 0;
 }
