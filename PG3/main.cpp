bd1642b0549470e51e0431fa9e67f317336e5eab
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-13 10:16:40 +0900

    数値野獣化

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,47 @@
 ﻿#include <iostream>
 #include <Windows.h>
+#include <stdio.h>
+
+template <typename T1, typename T2>
+class MyMath {
+public:
+
+	static double Min(T1 a, T2 b);
+};
+
+template<typename T1, typename T2>
+double  MyMath<T1, T2>::Min(T1 a, T2 b)
+{
+
+	if (static_cast<double>(a) < static_cast<double>(b)) {
+		return static_cast<double>(a);
+	}
+
+	return static_cast<double>(b);
+}
 
 int main() {
 
-	char str[] = "あ";
-	printf("%s", str);
-	return 0;
+	double result = 0.0;
 
+	result = MyMath<int, int>::Min(8, 10);
+	printf("%lf\n", result);
+
+	result = MyMath<int, float>::Min(8, 10.0f);
+	printf("%lf\n", result);
+
+	result = MyMath<int, double>::Min(10, 8.1);
+	printf("%lf\n", result);
+
+	result = MyMath<float, float>::Min(0.8f, 0.1f);
+	printf("%lf\n", result);
+
+	result = MyMath<float, double>::Min(8.1f, 8.2);
+	printf("%lf\n", result);
+
+	result = MyMath<double, double>::Min(81.0, 8.1);
+	printf("%lf\n", result);
+
+
+	return 0;
 }
