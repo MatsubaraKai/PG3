cffbf0fcf82aa4c2f86f56c46bfb6d4722a99d76
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-20 10:07:30 +0900

    01_04

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,86 +1,55 @@
-﻿#include <iostream>
+﻿#include <stdio.h>
 #include <Windows.h>
-#include <stdio.h>
-#include <ctime>
 #include <cstdlib>
 #include <functional>
+#include <ctime>
 
 typedef void (*PFunc)(int*);
 
-void DiceResult(int* userInput) {
-
-	int result = 0;
-	int answer = 0;
-
-	// ランダムで結果を決める
-	std::srand(static_cast<unsigned int>(time(NULL)));
-	result = rand() % 6 + 1;
-	answer = result % 2;
-
-	// 結果表示
-	if (answer == 1) {
-		printf("%d。奇数でした。\n", result);
+void SetTimeOut(std::function <void()> func, int second)
+{
+	for (second; second > 0; second--)
+	{
+		printf("*\n");
+		Sleep(1000);
 	}
-	else {
-		printf("%d。偶数でした。\n", result);
-	}
-
-	if (answer == *userInput) {
-		printf("あたりです\n");
-	}
-	else {
-		printf("はずれです\n");
-	}
-}
-
-void SetTimeOut(int second, std::function<void(int*)> func, int* funcArgument) {
-
-	Sleep(second * 1000);
-
-	func(funcArgument);
-
+	func();
 }
 
 int main() {
+	int Input = 0;
 
-	int userInput = 0;
-
-	// 奇数か偶数入力受付
+	// 入力受付
 	printf("偶数なら0、奇数なら1を入力\n");
 	do {
-		scanf_s("%d", &userInput);
-	} while (userInput != 0 && userInput != 1);
+		scanf_s("%d", &Input);
+	} while (Input != 0 && Input != 1);
 
 	// 結果表示
-	printf("結果は");
-	std::function<void(int*)> result = [](int* userInput) {
+	printf("結果は\n");
+	std::function<void()> func = [=]() {
 
 		int result = 0;
 		int answer = 0;
 
-		// ランダムで結果を決める
 		std::srand(static_cast<unsigned int>(time(NULL)));
 		result = rand() % 6 + 1;
 		answer = result % 2;
 
-		// 結果表示
 		if (answer == 1) {
 			printf("%d。奇数でした。\n", result);
 		}
 		else {
 			printf("%d。偶数でした。\n", result);
 		}
-
-		if (answer == *userInput) {
+		if (answer == Input) {
 			printf("あたりです\n");
 		}
 		else {
 			printf("はずれです\n");
 		}
+
 	};
-
-	// 三秒待つ
-	SetTimeOut(3, result, &userInput);
-
+	SetTimeOut(func, 3);
 	return 0;
 }
