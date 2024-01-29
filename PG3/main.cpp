86050071851d8e894de0b7299ed5621e40590a34
MatsubaraKai <k022g0104@g.neec.ac.jp>
2024-01-29 12:35:19 +0900

    a

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,29 @@
-﻿#include <iostream>
-#include <Windows.h>
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+using namespace std;
 
 int main() {
 
-	char str[] = "あ";
-	printf("%s", str);
-	return 0;
+	chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
+	string a(100000, 'a');
+
+	//copy
+	begin = chrono::steady_clock::now();
+	string copy = a;
+	chrono::steady_clock::time_point copyTimePoint = chrono::steady_clock::now();
+	chrono::microseconds copyTime = chrono::duration_cast<chrono::microseconds>(copyTimePoint - begin);
+
+	//move
+	begin = chrono::steady_clock::now();
+	string move = std::move(a);
+	chrono::steady_clock::time_point moveTimePoint = chrono::steady_clock::now();
+	chrono::microseconds moveTime = chrono::duration_cast<chrono::microseconds>(moveTimePoint - begin);
+
+	cout << "copy : " << copyTime.count() << " microsecond" << endl;
+	cout << "move : " << moveTime.count() << " microsecond" << endl;
+
+	return 0;
 }
